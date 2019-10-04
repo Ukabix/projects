eb801ce7fe4051da962aa446d08184b3da1c63e2
Ukabix <54454097+Ukabix@users.noreply.github.com>
2019-10-04 11:20:48 +0200

    Create run.py

---
+++ python/labirynt/run.py
@@ -0,0 +1,15 @@
+import time
+from foo import *
+
+print("Witaj w prostej grze, w której Twoim zadaniem jest znalezienie klucza, drzwi i wydostanie się z niego")
+print("Jesteś przy wejściu do labiryntu.")
+
+name = input("Jak się nazywasz?")
+print ("Witaj " + name)
+time.sleep(3)
+print("Zaczynasz się budzić, gdy uderzasz w ziemię. Boli. Gdzie jesteś? To jakieś więzienie.")
+print("Znajdujesz się w " + lok8)
+
+lok8 = ("Znajdujesz się w płd pokoju. Pokój rozświetlają promienie słońca dostające się tutaj przez otwór w suficie. Ten sam otwór, przez który Cię tutaj zrzucono.")
+
+a = input()
