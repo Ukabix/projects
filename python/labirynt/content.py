a32617afccc070f1de197c39c37d023b5d9531fd
Ukabix <54454097+Ukabix@users.noreply.github.com>
2019-10-04 11:20:41 +0200

    Create content.py

---
+++ python/labirynt/content.py
@@ -0,0 +1,14 @@
+
+  
+# Informacja o lokalizacji
+1 = print("Znajdujesz się w płn-zach pokoju")
+2 = print("Znajdujesz się w płn pokoju, widzisz zamknięte drzwi i otwór na klucz")
+3 = print("Znajdujesz się w płn-wsch pokoju")
+4 = print("Znajdujesz się w zach pokoju")
+5 = print("Znajdujesz się w centralnym pokoju")
+6 = print("Znajdujesz się we wschodnim pokoju")
+7 = print("Znajdujesz się w płd-wsch pokoju")
+8 = print("Znajdujesz się w płd pokoju. Pokój rozświetlają promienie słońca dostające się tutaj przez otwór w suficie. Ten sam otwór, przez który Cię tutaj zrzucono.")
+9 = print("Znajdujesz się w płd-wsch pokoju.")
+
+
